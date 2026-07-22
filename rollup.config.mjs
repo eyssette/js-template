acb317e9cc8094261202690bd865d32fdda4ad8a
Cédric Eyssette <cedric.eyssette@ac-lyon.fr>
2026-07-22 12:53:03 +0200

    fix(rollup): ajout d'un délai pour permettre le livereload

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -87,6 +87,6 @@ export default {
 
 		// En mode développement, lance un serveur de développement et recharge la page automatiquement lorsqu'un fichier est modifié
 		development && serve({ contentBase: ["dist", "./"], open: true }),
-		development && livereload(),
+		development && livereload({ delay: 300 }),
 	],
 };
