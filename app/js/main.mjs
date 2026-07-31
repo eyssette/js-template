e90d80bb5d819d50f93fe0e1e89d5aa03a22fce1
Cédric Eyssette <cedric.eyssette@ac-lyon.fr>
2026-07-31 08:13:58 +0200

    fix(i18n): correctif pour l'exemple de la page d'accueil
    On a besoin pour l'exemple simple que de getLocale()

--- app/js/main.mjs
+++ app/js/main.mjs
@@ -1,12 +1,13 @@
 // oxlint-disable import/no-unassigned-import import/no-relative-parent-imports
 import "../css/styles.css";
-import { getLocale, setLocale } from "./lib/paraglide/runtime.js";
+import { getLocale } from "./lib/paraglide/runtime.js";
 import { capitalizeFirstLetter } from "./utils/strings.mjs";
 import { m } from "./lib/paraglide/messages.js";
 
-setLocale("fr");
-
-const mainElement = document.querySelector("main");
 const currentLocale = getLocale();
 document.documentElement.lang = currentLocale;
-mainElement.textContent = capitalizeFirstLetter(`hello world ! ${m.welcome()}`);
+
+const mainElement = document.querySelector("main");
+const message = m.welcome();
+
+mainElement.textContent = capitalizeFirstLetter(`hello world ! ${message}`);
